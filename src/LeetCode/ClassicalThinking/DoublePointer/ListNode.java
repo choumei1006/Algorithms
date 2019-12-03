626858f33ebca3c0b252da03cc62097ba3a031c5
choumei1006 <1479784599@qq.com>
2019-12-03 22:30:54 +0800

    141：Linked list cycle;

---
+++ src/LeetCode/ClassicalThinking/DoublePointer/ListNode.java
@@ -0,0 +1,15 @@
+package LeetCode.ClassicalThinking.DoublePointer;
+
+/**
+ * @author:choumei
+ * @date:2019/12/3 22:14
+ * @Description: Definition for singly-linked list.
+ */
+public class ListNode {
+    int val;
+     ListNode next;
+     ListNode(int x) {
+          val = x;
+          next = null;
+     }
+}
