5d45ff17f02191f56d053f11952db2ac0c4e145e
choumei1006 <1479784599@qq.com>
2019-12-19 21:59:06 +0800

    617：Merge two binary trees.

--- src/LeetCode/DataStructures/Tree/Recurrence/InvertBinaryTree_226.java
+++ src/LeetCode/DataStructures/Tree/Recurrence/InvertBinaryTree_226.java
@@ -27,10 +27,11 @@ import PublicClasses.TreeNode;
  * 这个问题是受到 Max Howell 的 原问题 启发的 ：
  *
  * 谷歌：我们90％的工程师使用您编写的软件(Homebrew)，但是您却无法在面试时在白板上写出翻转二叉树这道题，这太糟糕了。
- *
+ *`
  */
 public class InvertBinaryTree_226 {
     public static void main(String[] args) {
+
         TreeNode l2_1 = new TreeNode(2);
         TreeNode l2_2 = new TreeNode(3);
         TreeNode l3_1 = new TreeNode(4);
