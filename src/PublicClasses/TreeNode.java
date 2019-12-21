49b847b611a9a3cb3be239710d8ed8e0400c97e8
choumei1006 <1479784599@qq.com>
2019-12-21 13:04:11 +0800

    437：Path sum III.

--- src/PublicClasses/TreeNode.java
+++ src/PublicClasses/TreeNode.java
@@ -1,15 +1,49 @@
 package PublicClasses;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * @author:choumei
  * @date:2019/12/15 16:33
  * @Description: Definition for a binary tree node.
  */
 public class TreeNode {
-    public int val;
+    public Integer val;
     public TreeNode left;
     public TreeNode right;
-    public TreeNode(int x){
+
+    public TreeNode(Integer x){
+        if(null == x) return;
         this.val = x;
+    }
+
+    public static TreeNode initTree(TreeNode[] list){
+        if(null == list) return null;
+        if(list.length <= 1) return list[0];
+        int i = 0,j = 1;
+        while(j+1<list.length){
+            list[i].left = list[j];
+            list[i].right = list[j+1];
+
+            i++;
+            j+=2;
+        }
+        if(j+1 == list.length){
+            list[i].left = list[j];
+        }
+        return list[0];
+    }
+
+    public static TreeNode[] initTreeNodeList(Integer[] list){
+        TreeNode[] nodeList = new TreeNode[list.length];
+        for (int i = 0; i < list.length ; i++) {
+            if(null == list[i]){
+                nodeList[i] = null;
+            }else {
+                nodeList[i] = new TreeNode(list[i]);
+            }
+        }
+        return nodeList;
     }
 }
