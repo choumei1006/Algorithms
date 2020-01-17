caef484b166b10d8a7b08a504d2f47f56b3e06bc
choumei1006 <1479784599@qq.com>
2020-01-17 20:59:06 +0800

    [Review]696: Count binary substrings.

--- src/LeetCode/DataStructures/String/CountBinarySubstrings_696.java
+++ src/LeetCode/DataStructures/String/CountBinarySubstrings_696.java
@@ -3,8 +3,8 @@ package LeetCode.DataStructures.String;
 /**
  * @author:choumei
  * @date:2020/1/16 22:36
- * @Description: 给定一个字符串 s，计算具有相同数量0和1的非空(连续)子字符串的数量，并且这些子字符串中的所有0和所有1都是组合在一起的。
- *
+ * @Description: 给定一个字符串 s，计算具有相同数量0和1的非空(连续)子字符串的数量，
+ * 并且这些子字符串中的所有0和所有1都是组合在一起的。
  * 重复出现的子串要计算它们出现的次数。
  *
  * 示例 1 :
@@ -32,6 +32,22 @@ public class CountBinarySubstrings_696 {
         System.out.println(countBinarySubstrings("00110011"));
     }
     public static int countBinarySubstrings(String s){
+        int preLen = 0,curLen = 1,count = 0;
+        for (int i = 1; i < s.length() ; i++) {
+            if(s.charAt(i) == s.charAt(i-1)){
+                curLen++;
+            }else{
+                preLen = curLen;
+                curLen = 1;
+            }
+            if(preLen >= curLen){
+                count++;
+            }
+        }
+        return count;
+    }
+    //repeat
+    public static int countBinarySubstrings_repeat(String s){
         int preLen = 0,curLen = 1,count = 0;
         for (int i = 1; i < s.length() ; i++) {
             if(s.charAt(i) == s.charAt(i-1)){
