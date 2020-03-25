b21884d0a3a272b85289e4fd4737d47d3b334fb9
choumei1006 <1479784599@qq.com>
2020-03-25 23:48:47 +0800

    4:Median of sorted arrays.

---
+++ src/LeetCode/ClassicalThinking/BackTrack/NQueueProblems.java
@@ -0,0 +1,111 @@
+package LeetCode.ClassicalThinking.BackTrack;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @author:choumei
+ * @date:2020/3/25 14:40
+ * @Description:
+ */
+public class NQueueProblems {
+    @Test
+    public void testNQueues(){
+        List<List<String>> res = solveNQueues(8);
+        int idx = 1;
+        for(List<String> list : res){
+            System.out.println("方案："+ idx++);
+            for(String str : list){
+                System.out.println("["+str+"]");
+            }
+        }
+    }
+
+    //结果集
+    List<List<String>> res = new LinkedList<>();
+    //N皇后问题
+    public List<List<String>> solveNQueues(int n){
+        int[][] board = new int[n][n];
+        backTrack(board,0);
+        return res;
+    }
+
+
+    /**
+     * 回溯
+     * @param board
+     * @param rowIdx
+     */
+    public void backTrack(int[][] board, int rowIdx){
+        int queenSize = board.length;
+        if(rowIdx == queenSize){
+            res.add(format(board));
+            return;
+        }
+
+        for(int col = 0; col < queenSize; col++){
+            if(!isValid(board,rowIdx,col)){
+                continue;
+            }
+            //决策
+            board[rowIdx][col] = 1;
+            //进入下一行决策
+            backTrack(board, rowIdx + 1);
+            //撤销本次决策
+            board[rowIdx][col] = 0;
+        }
+    }
+
+    /**
+     * 判断当前坐标是否适合放置皇后
+     * @param board
+     * @param rowIdx
+     * @param colIdx
+     * @return
+     */
+    public boolean isValid(int[][] board, int rowIdx, int colIdx){
+        int queenSize = board.length;
+        //检查所在列是否有皇后冲突
+        for(int i = 0; i < rowIdx; i++){
+            if(board[i][colIdx] == 1){
+                return false;
+            }
+        }
+        //判断左上方是否有皇后冲突
+        for(int i = rowIdx-1, j = colIdx-1; i >= 0 && j >= 0; i--,j--){
+            if(board[i][j] == 1){
+                return false;
+            }
+        }
+        //判断右上方是否有皇后冲突
+        for(int i = rowIdx-1, j = colIdx+1; i >= 0 && j < queenSize; i--,j++){
+            if(board[i][j] == 1){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 将棋盘数据转换为‘Q’‘.’组成的集合
+     * @param nums
+     * @return
+     */
+    public List<String> format(int[][] nums){
+        int n = nums.length;
+        List<String> res = new LinkedList<>();
+        for(int i = 0; i < n; i++){
+            StringBuilder curStr = new StringBuilder();
+            for(int j = 0; j < n; j++){
+                if(nums[i][j] == 1){
+                    curStr.append('Q');
+                }else{
+                    curStr.append('.');
+                }
+            }
+            res.add(curStr.toString());
+        }
+        return res;
+    }
+}
---
+++ src/LeetCode/ClassicalThinking/BackTrack/PermuteTest.java
@@ -0,0 +1,50 @@
+package LeetCode.ClassicalThinking.BackTrack;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author:choumei
+ * @date:2020/3/25 13:02
+ * @Description: 数组全排列问题
+ */
+public class PermuteTest {
+    @Test
+    public void test(){
+        List<List<Integer>> lists = permute(new int[]{1, 2, 3});
+        for(List tempList : lists){
+            System.out.println(tempList.toString());
+        }
+    }
+    //所有排列集合
+
+    private List<List<Integer>> res = new LinkedList<>();
+    //全排列
+    public List<List<Integer>> permute(int[] nums){
+        //已访问路径
+        LinkedList<Integer> track = new LinkedList<>();
+        //访问
+        backTrack(nums,track);
+        return res;
+    }
+    //回溯函数
+    public void backTrack(int[] nums, LinkedList<Integer> track){
+        //是否满足一个排列
+        if(track.size() == nums.length){
+            res.add(new LinkedList<>(track));
+            return;
+
+        }
+        for(int i = 0; i < nums.length; i++){
+            int curr = nums[i];
+            if(track.contains(curr)){
+                continue;
+            }
+            track.add(curr);
+            backTrack(nums,track);
+            track.removeLast();
+        }
+    }
+}
