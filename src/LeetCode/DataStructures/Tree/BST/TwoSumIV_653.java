5e80149a4dc0ec436d8529cfe0b9be2c249b1528
choumei1006 <1479784599@qq.com>
2020-04-17 22:59:21 +0800

    4:Median of sorted arrays.

--- src/LeetCode/DataStructures/Tree/BST/TwoSumIV_653.java
+++ src/LeetCode/DataStructures/Tree/BST/TwoSumIV_653.java
@@ -8,7 +8,10 @@ import java.util.List;
 /**
  * @author:choumei
  * @date:2019/12/31 21:54
- * @Description: 给定一个二叉搜索树和一个目标结果，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+ * @Description:
+ * 给定一个二叉搜索树和一个目标结果，
+ * 如果 BST 中存在两个元素且它们的和等于给定的目标结果，
+ * 则返回 true。
  *
  * 案例 1:
  *
