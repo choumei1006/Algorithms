d41f7677944c4d512ac4cd2a0e2ed53e3787c54d
choumei1006 <1479784599@qq.com>
2020-04-18 22:37:47 +0800

    4:Median of sorted arrays.

--- src/LeetCode/ClassicalThinking/DoublePointer/TrapWater_42.java
+++ src/LeetCode/ClassicalThinking/DoublePointer/TrapWater_42.java
@@ -1,4 +1,4 @@
-package Demo.DP;
+package LeetCode.ClassicalThinking.DoublePointer;
 
 import org.junit.Test;
 
@@ -88,12 +88,32 @@ public class TrapWater_42 {
      * @return
      */
     public int trap3(int[] height){
-        if(null == height || height.length == 0){
-            return 0;
+        int sum = 0;
+        int max_left = 0;
+        int max_right = 0;
+        int left = 1;
+        int right = height.length - 2; // 加右指针进去
+        for (int i = 1; i < height.length - 1; i++) {
+            //从左到右更
+            if (height[left - 1] < height[right + 1]) {
+                max_left = Math.max(max_left, height[left - 1]);
+                int min = max_left;
+                if (min > height[left]) {
+                    sum = sum + (min - height[left]);
+                }
+                left++;
+                //从右到左更
+            } else {
+                max_right = Math.max(max_right, height[right + 1]);
+                int min = max_right;
+                if (min > height[right]) {
+                    sum = sum + (min - height[right]);
+                }
+                right--;
+            }
         }
-        int leftPoint = 1;
-        int rightPoint = height.length-2;
-        return  0;
+        return sum;
+
 
 
     }
