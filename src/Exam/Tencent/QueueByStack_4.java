d5ce8dd648b9d2afae105b846da8ad93d7ca6cf7
choumei1006 <1479784599@qq.com>
2020-05-12 22:39:47 +0800

    4:Median of sorted arrays.

--- src/Exam/Tencent/QueueByStack_4.java
+++ src/Exam/Tencent/QueueByStack_4.java
@@ -5,7 +5,34 @@ import java.util.*;
 /**
  * @author:choumei
  * @date:2020/4/26 20:43
- * @Description:
+ * @Description: 用两个栈实现队列，支持队列的基本操作：
+ *
+ * 输入描述:
+ * 第一行输入一个整数N，表示对队列进行的操作总数。
+ * 下面N行每行输入一个字符串S，表示操作的种类。
+ * 如果S为“add”,则后面还有一个整数X表示向队列尾部加入整数X；
+ * 如果S为“poll”,则表示弹出队列头部操作；
+ * 如果S为“peek”,则表示询问当前队列中头部元素是多少。
+ *
+ * 数据保证没有不合法的操作：
+ *
+ * 输出描述：
+ * 对于每一个为“peek”的操作，输出一行表示当前队列中头部元素是多少。
+ *
+ * 示例1：
+ *
+ * 输入：
+ * 6
+ * add 1
+ * add 2
+ * add 3
+ * peek
+ * poll
+ * peek
+ *
+ * 输出：
+ * 1
+ * 2
  */
 public class QueueByStack_4 {
     public static void main(String[] args) {
