326c37f3df80d7870d2136284b64da76e698737e
choumei1006 <1479784599@qq.com>
2020-05-20 12:06:43 +0800

    4:Median of sorted arrays.

--- src/LeetCode/DataStructures/Array/KSequenceOddSubarrays_1248.java
+++ src/LeetCode/DataStructures/Array/KSequenceOddSubarrays_1248.java
@@ -8,7 +8,9 @@ import java.util.List;
 /**
  * @author:choumei
  * @date:2020/4/21 9:34
- * @Description: 给你一个整数数组 nums 和一个整数 k。
+ * @Description:  统计【优美子数组】
+ * 
+ * 给你一个整数数组 nums 和一个整数 k。
  *
  * 如果某个 连续 子数组中恰好有 k 个奇数数字，我们就认为这个子数组是「优美子数组」。
  *
@@ -42,12 +44,12 @@ import java.util.List;
 public class KSequenceOddSubarrays_1248 {
     @Test
     public void test(){
-        System.out.println(numOfSubarrays(new int[]{1,1,2,1,1},3));
+        System.out.println(numOfSubarrays2(new int[]{1,1,2,1,1},3));
     }
 
     /**
      * 求出拥有K个奇数的连续子数组
-     * 求出奇数位置下标，子数组个数 = 每两个奇数围起来的区间左右偶数数目之积
+     * 求出奇数位置下标，子数组个数 = 每相隔K的两个奇数围起来的区间左右偶数数目之积
      * @param nums
      * @param k
      * @return
@@ -77,5 +79,42 @@ public class KSequenceOddSubarrays_1248 {
                     ( oddList.get(i + k) - oddList.get(i + k - 1));
         }
         return res;
+    }
+
+    /**
+     * 自测
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int numOfSubarrays2(int[] nums, int k){
+        if(null == nums || nums.length == 0 || k > nums.length){
+            return 0;
+        }
+        int len = nums.length;  
+        
+        int resCnt = 0;    //结果集
+        List<Integer> oddList = new ArrayList<Integer>();  //奇数下标数组
+        
+        oddList.add(-1);   //初始化左边界
+
+        
+        for (int i = 0; i < len; i++) {
+            if(nums[i] % 2 != 0){
+                oddList.add(i);
+            }
+        }
+        
+        oddList.add(len);
+        
+        //奇数总数不足k
+        if(oddList.size() < k + 2){
+            return 0;
+        }
+
+        for (int i = 1; i < oddList.size() - k; i++) {
+            resCnt += (oddList.get(i) - oddList.get(i - 1)) * (oddList.get(i + k) - oddList.get(i + k - 1));
+        }
+        return resCnt;
     }
 }
