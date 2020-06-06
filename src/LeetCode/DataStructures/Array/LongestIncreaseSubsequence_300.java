dc846384cfa6dd0d795c46ae21e2efab180c79f2
choumei1006 <1479784599@qq.com>
2020-06-06 11:43:28 +0800

    4:Median of sorted arrays.

--- src/LeetCode/DataStructures/Array/LongestIncreaseSubsequence_300.java
+++ src/LeetCode/DataStructures/Array/LongestIncreaseSubsequence_300.java
@@ -1,5 +1,7 @@
 package LeetCode.DataStructures.Array;
 
+import org.junit.Test;
+
 /**
  * @author:choumei
  * @date:2020/3/14 22:59
@@ -48,5 +50,46 @@ public class LongestIncreaseSubsequence_300 {
             rstLen = Math.max(rstLen,dp[i]);
         }
         return rstLen;
+    }
+
+
+    /**
+     * 自测
+     */
+    @Test
+    public void test(){
+        System.out.println(LengthOfLIS2(new int[]{10,9,2,5,3,7,101,18}));
+    }
+
+    /**
+     * 动态规划：返回最长上升序列长度
+     * @param nums
+     * @return
+     */
+    public int LengthOfLIS2(int[] nums){
+        //特判
+        if(null == nums || nums.length == 0){
+            return 0;
+        }
+
+        int len = nums.length;
+        int[] dp = new int[len];  //记录以nums[i]结尾的最长上升序列长度
+        dp[0] = 1;
+
+        int resLen = 1;
+
+        //设置1->len-1的dp值
+        for (int i = 1; i < len; i++) {
+            int tmpLen = 0;   //nums[i]之前的元素结尾的最长上升序列长度
+            for (int j = 0; j < i; j++) {  //遍历0-> i-1 区间上元素
+                if(nums[j] < nums[i]){
+                    //若该元素比nums[i]小，则比较以nums[j]结尾的最长上升子序列长度是否超过当前tmpLen
+                    tmpLen = Math.max(tmpLen, dp[j]);
+                }
+            }
+            dp[i] = tmpLen + 1;   //加当前nums[i]元素
+            resLen = Math.max(resLen, dp[i]);  //更新结果长度
+        }
+        return resLen;
     }
 }
