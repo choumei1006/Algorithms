11f859d055fb5d57ed8f3b4c14dbeb3c8b110056
choumei1006 <1479784599@qq.com>
2020-08-06 21:26:36 +0800

    20200806 Review

---
+++ src/Exam/JD/SumOfN.java
@@ -0,0 +1,32 @@
+package Exam.JD;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ * @author:choumei
+ * @date:2020/8/6 20:11
+ * @Description:
+ */
+public class SumOfN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(sumOfN(n));
+    }
+
+    /**
+     * 计算给定数列前 n 项和
+     * @param n
+     * @return
+     */
+    public static String sumOfN(int n){
+        double tmp = 0;
+        for (int i = 1; i <= n; i++) {
+            tmp += (1.0 / (5 * (2 * i - 1))) - (1.0 / (5 * (2 * i )));
+        }
+        System.out.println(tmp);
+        return String.format("%.4f", tmp);
+    }
+
+}
